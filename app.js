19aa8a5f955753f4b2e15a4cf5b332b4b1191a53
SatsangSeva <satsangsevaonline@gmail.com>
2024-09-11 17:17:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ dotenv.config();
 const app = express();
 
 // middlewares
-app.use(cors());
+const corsOptions = {
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
